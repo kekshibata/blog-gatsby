00ae8d7ddeb9587b5110492cbedfc4791affd5d6
Koki Shibata <kenergyintense@gmail.com>
2021-11-10 04:50:46 +0900

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,41 +6,18 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
-    'airbnb/hooks',
-    'plugin:import/errors',
-    'plugin:import/warnings',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 2020,
     sourceType: 'module',
   },
   plugins: [
-    'import',
-    'jsx-a11y',
     'react',
-    'react-hooks',
   ],
   rules: {
     'react/prop-types': 0,
-    'import/prefer-default-export': 0,
-    'padding-line-between-statements': [
-      'error',
-      {
-        blankLine: 'always',
-        prev: '*',
-        next: 'return',
-      },
-    ],
-    'react/jsx-props-no-spreading': [
-      'error',
-      {
-        html: 'enforce',
-        custom: 'enforce',
-        explicitSpread: 'ignore',
-      },
-    ],
   },
 };
